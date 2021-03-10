f2ba2d774477a0ba6e0170f5c47e6ecfe2d54fd5
Dennis <b976543257@gmail.com>
2021-03-11 01:39:46 +0800

    3

--- tests/unit/__tests__/test.js
+++ tests/unit/__tests__/test.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+import basic from "../data/basic.json";
 
 let serverOptions = {
   headless: false,
@@ -24,6 +25,7 @@ describe("labeling-tool", () => {
         console.log(`${i}: ${text}`);
       }
     });
+    await page.emulateMediaType("screen");
   });
   afterAll(() => {
     browser.close();
@@ -90,19 +92,20 @@ describe("labeling-tool", () => {
       await page.keyboard.press(keyCode);
       await page.waitFor(waitForTimeOut);
     },
-    waitForLoading: async (element) => {
-      await page.waitForFunction(
-        (element) => !document.querySelector(element),
-        {
-          timeout: 50000,
-        },
-        element
-      );
-      await page.waitFor(waitForTimeOut);
-    },
-    getPDF: async (name) => {
+    getPDF: async (name, options = {}) => {
       if (headless) {
-        await page.pdf({ path: `./pdf/${name}.pdf` });
+        await page.pdf({
+          path: `./pdf/${name}.pdf`,
+          printBackground: true,
+          format: "A4",
+          margin: {
+            top: "10px",
+            bottom: "10px",
+            right: "10px",
+            left: "10px",
+          },
+          ...options,
+        });
         await page.waitFor(waitForTimeOut);
       }
     },
@@ -113,149 +116,219 @@ describe("labeling-tool", () => {
       });
     },
   };
-  // it("取得JSON網址", async () => {
-  //   await utils.goto("https://monospace.kktix.cc/events.json");
-  //   await page.evaluate;
-  //   url = await page.evaluate(() => {
-  //     let url, json;
-  //     json = JSON.parse(document.querySelector("body").innerText);
-  //     json["entry"].forEach((item) => {
-  //       Object.keys(item).forEach((key) => {
-  //         if (key === "title") {
-  //           if (item[key].includes("0xFE")) {
-  //             url = item["url"];
-  //             return;
-  //           }
-  //         }
-  //       });
-  //     });
-  //     return url;
-  //   });
-  //   await page.waitFor(1000);
-  //   console.log(url);
-  //   await utils.getScreenShot("取得JSON網址.png", { fullPage: true });
-  //   await utils.getPDF("取得JSON網址");
-  // });
-  // it("登入活動頁面", async () => {
-  //   await utils.goto(url);
-  //   spanElement = await utils.$eval(
-  //     "body > div.outer-wrapper > div.content-wrapper > div > div.og-banner > img",
-  //     "src"
-  //   );
-  //   console.log(spanElement);
-  //   code = await page.evaluate(() => {
-  //     let code = document.querySelector(
-  //       "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > pre"
-  //     ).innerText;
-  //     code = code.slice(14, 30);
-  //     return code;
-  //   });
-  //   console.log(code);
-  //   title = await page.evaluate(() => {
-  //     let title = document.querySelector(
-  //       "body > div.outer-wrapper > div.content-wrapper > div > div.header > div > h1"
-  //     ).innerText;
-  //     return title;
-  //   });
-  //   console.log(title);
-  //   content = await page.evaluate(() => {
-  //     let content = document.querySelector(
-  //       "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > h2"
-  //     ).innerText;
-  //     content +=
-  //       " " +
-  //       document.querySelector(
-  //         "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > p:nth-child(2)"
-  //       ).innerText;
-  //     content +=
-  //       " " +
-  //       document.querySelector(
-  //         "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > p:nth-child(4)"
-  //       ).innerText;
-  //     content +=
-  //       " " +
-  //       document.querySelector(
-  //         "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > pre"
-  //       ).innerText;
-  //     return content;
-  //   });
-  //   console.log(content);
-  //   await utils.getScreenShot("登入活動頁面.png", { fullPage: true });
-  //   await utils.getPDF("登入活動頁面");
-  // });
-  // it("data-code解碼", async () => {
-  //   await utils.goto(
-  //     "https://www.convertstring.com/zh_TW/EncodeDecode/Base64Decode"
-  //   );
-  //   await utils.type("#input", code);
-  //   await utils.click("#submit");
-  //   await page.waitFor(5000);
-  //   code = await page.evaluate(() => {
-  //     let code;
-  //     code = document.querySelector("#output").value;
-  //     return code;
-  //   });
-  //   console.log(code);
-  //   await utils.getScreenShot("ata-code解碼.png", { fullPage: true });
-  //   await utils.getPDF("ata-code解碼");
-  // });
-  // it("填寫票數", async () => {
-  //   await utils.goto(url);
-  //   await utils.click(
-  //     "body > div.outer-wrapper > div.content-wrapper > div > div.tickets > a"
-  //   );
-  //   await utils.click(
-  //     "#guestModal > div.modal-dialog > div > div.modal-footer > button"
-  //   );
-  //   await utils.click(
-  //     "#ticket_344407 > div > span.ticket-quantity.ng-scope > input"
-  //   );
-  //   await utils.click(
-  //     "#ticket_344407 > div > span.ticket-quantity.ng-scope > button.btn-default.plus"
-  //   );
-  //   await utils.click("#person_agree_terms");
-  //   await utils.getScreenShot("填寫票數.png", { fullPage: true });
-  //   await utils.getPDF("填寫票數");
-
-  //   await utils.click(
-  //     "#registrationsNewApp > div > div:nth-child(5) > div.form-actions.plain.align-center.register-new-next-button-area"
-  //   );
-  // });
-  // it("填寫表單", async () => {
-  //   await utils.click(
-  //     "#guestModal > div.modal-dialog > div > div.modal-footer > button"
-  //   );
-  //   await utils.type("#field_text_701843 > div > div > input", "蔡名彥");
-  //   await utils.type(
-  //     "#field_email_701844 > div > div > input",
-  //     "b976543257@gmail.com"
-  //   );
-  //   await utils.type("#field_text_701845 > div > div > input", "0912345678");
-  //   await utils.type("#field_text_701846 > div > div > input", code);
-  //   await utils.click("#person_agree_terms");
-  //   await utils.getScreenShot("填寫表單.png", { fullPage: true });
-  //   await utils.getPDF("填寫表單");
-  //   await utils.click(
-  //     "#registrations_controller > div:nth-child(4) > div:nth-child(2) > div > div.form-actions.plain.align-center.ng-scope > a"
-  //   );
-  //   await page.waitFor(5000);
-  //   await utils.getScreenShot("完成訂單.png", { fullPage: true });
-  //   await utils.getPDF("完成訂單");
-  // });
-  it("填寫票數", async () => {
-    await page.addScriptTag({
-      path: `./node_modules/easy-pdf-merge`,
+  it("取得JSON網址", async () => {
+    await utils.goto("https://monospace.kktix.cc/events.json");
+    url = await page.evaluate(() => {
+      let url, json;
+      json = JSON.parse(document.querySelector("body").innerText);
+      json["entry"].forEach((item) => {
+        Object.keys(item).forEach((key) => {
+          if (key === "title") {
+            if (item[key].includes("0xFE")) {
+              url = item["url"];
+              return;
+            }
+          }
+        });
+      });
+      return url;
     });
-    // await page.evaluate(() => {
-    //   window.merge(
-    //     ["../../../pdf/完成訂單.pdf", "../../../pdf/取得JSON網址.pdf"],
-    //     "a.pdf",
-    //     function(err) {
-    //       if (err) return console.log(err);
-    //       console.log("Successfully merged!");
-    //     }
-    //   );
-    // });
-    console.log(`${process.cwd()}`);
+    await page.waitFor(waitForTimeOut);
+    console.log(url);
+    await utils.getScreenShot("第一步驟-取得JSON網址.png", { fullPage: true });
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第一步驟";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第一步驟-取得JSON網址");
   });
+  it("登入活動頁面", async () => {
+    await utils.goto(url);
+    spanElement = await utils.$eval(
+      "body > div.outer-wrapper > div.content-wrapper > div > div.og-banner > img",
+      "src"
+    );
+    console.log(spanElement);
+    code = await page.evaluate(() => {
+      let code = document.querySelector(
+        "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > pre"
+      ).innerText;
+      code = code.slice(14, 30);
+      return code;
+    });
+    await page.waitFor(waitForTimeOut);
+    console.log(code);
+    title = await page.evaluate(() => {
+      let title = document.querySelector(
+        "body > div.outer-wrapper > div.content-wrapper > div > div.header > div > h1"
+      ).innerText;
+      return title;
+    });
+    await page.waitFor(waitForTimeOut);
+    console.log(title);
+    content = await page.evaluate(() => {
+      let content = document.querySelector(
+        "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > h2"
+      ).innerText;
+      content +=
+        " " +
+        document.querySelector(
+          "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > p:nth-child(2)"
+        ).innerText;
+      content +=
+        " " +
+        document.querySelector(
+          "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > p:nth-child(4)"
+        ).innerText;
+      content +=
+        " " +
+        document.querySelector(
+          "body > div.outer-wrapper > div.content-wrapper > div > div.main.clearfix > div > pre"
+        ).innerText;
+      return content;
+    });
+    await page.waitFor(waitForTimeOut);
+    console.log(content);
+    await utils.getScreenShot("第二步驟-登入活動頁面.png", { fullPage: true });
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第二步驟";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第二步驟-登入活動頁面");
+  });
+  it("data-code解碼", async () => {
+    await utils.goto(
+      "https://www.convertstring.com/zh_TW/EncodeDecode/Base64Decode"
+    );
+    await utils.type("#input", code);
+    await utils.click("#submit");
+    await page.waitFor(waitForTimeOut);
+    code = await page.evaluate(() => {
+      let code;
+      code = document.querySelector("#output").value;
+      return code;
+    });
+    await page.waitFor(waitForTimeOut);
+    console.log(code);
+    await utils.getScreenShot("第三步驟-data-code解碼.png", { fullPage: true });
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第三步驟";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第三步驟-data-code解碼");
+  });
+  it("填寫票數", async () => {
+    await utils.goto(url);
+    await utils.click(
+      "body > div.outer-wrapper > div.content-wrapper > div > div.tickets > a"
+    );
+    await utils.click(
+      "#guestModal > div.modal-dialog > div > div.modal-footer > button"
+    );
+    await utils.click(
+      "#ticket_344407 > div > span.ticket-quantity.ng-scope > input"
+    );
+    await utils.click(
+      "#ticket_344407 > div > span.ticket-quantity.ng-scope > button.btn-default.plus"
+    );
+    await utils.click("#person_agree_terms");
+    await utils.getScreenShot("第四步驟-填寫票數.png", { fullPage: true });
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第四步驟";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第四步驟-填寫票數");
+
+    await utils.click(
+      "#registrationsNewApp > div > div:nth-child(5) > div.form-actions.plain.align-center.register-new-next-button-area"
+    );
+  });
+  it("填寫表單", async () => {
+    await utils.click(
+      "#guestModal > div.modal-dialog > div > div.modal-footer > button"
+    );
+    await utils.type(
+      "#field_text_701843 > div > div > input",
+      basic.basic[0].name
+    );
+    await utils.type(
+      "#field_email_701844 > div > div > input",
+      basic.basic[0].eMail
+    );
+    await utils.type(
+      "#field_text_701845 > div > div > input",
+      basic.basic[0].cellphone
+    );
+    await utils.type("#field_text_701846 > div > div > input", code);
+    await utils.click("#person_agree_terms");
+    await utils.getScreenShot("第五步驟-填寫表單.png", { fullPage: true });
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第五步驟";
+      title.id = "five";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第五步驟-填寫表單");
+    await utils.click(
+      "#registrations_controller > div:nth-child(4) > div:nth-child(2) > div > div.form-actions.plain.align-center.ng-scope > a"
+    );
+    await utils.getScreenShot("第六步驟-完成訂單.png", { fullPage: true });
+    await page.waitForSelector(
+      "#registrations_controller > div.ng-scope > div.narrow-wrapper.ng-scope > div > div.order-wrapper.ng-scope > div:nth-child(5) > div:nth-child(2) > div > ul > li > div > div > div.sub-block.ng-scope > div.qr-code.ng-scope > img",
+      {
+        visible: true,
+      }
+    );
+    await page.evaluate(() => {
+      const title = document.createElement("h1");
+      const body = document.querySelector("body");
+      title.textContent = "第六步驟";
+      title.style.textAlign = "center";
+      title.style.color = "red";
+      title.style.zIndex = "99999";
+      document.getElementById("five").remove();
+      body.insertBefore(title, body.childNodes[0]);
+    });
+    await utils.getPDF("第六步驟-完成訂單");
+  });
+  // it("填寫票數", async () => {
+  // await page.addScriptTag({
+  //   path: `./node_modules/easy-pdf-merge`,
+  // });
+  // await page.evaluate(() => {
+  //   window.merge(
+  //     ["../../../pdf/完成訂單.pdf", "../../../pdf/取得JSON網址.pdf"],
+  //     "a.pdf",
+  //     function(err) {
+  //       if (err) return console.log(err);
+  //       console.log("Successfully merged!");
+  //     }
+  //   );
+  // });
+  // console.log(`${process.cwd()}`);
+  // });
 });
